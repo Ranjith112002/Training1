3989204e5b8b958e69e5b6e4e3b27270291fdfc3
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-08-19 15:54:05 +0530

    A01:Guessing Number;

--- A01/Program.cs
+++ A01/Program.cs
@@ -1,15 +1,59 @@
-﻿Random random = new Random();
-int secretNumber = random.Next(1, 101);
-int guess = 0;
-Console.WriteLine("Guess a number between 1 and 100.");
-while (guess != secretNumber)
+﻿var mode = GetMode();
+Console.WriteLine(mode);
+int max = getMax(mode);
+int secret = Random.Shared.Next(1, max + 1);
+Console.WriteLine($"I've thought of a number between 1 to {max}.");
+Console.WriteLine("Try to guess it");
+for (int tries = 1; ; tries++)
 {
-    Console.Write("Enter your guess: ");
-    guess = int.Parse(Console.ReadLine());
-    if (guess < secretNumber)
-        Console.WriteLine("Too low!");
-    else if (guess > secretNumber)
-        Console.WriteLine("Too high!");
-    else
-        Console.WriteLine("Correct! You guessed the number.");
+    int guess = ReadInt("> ");
+    var result = CheckGuess(secret, guess);
+    if (result == Guess.Exact)
+    {
+        Console.WriteLine($"You guessed in {tries} tries.");
+        break;
+    }
+    Console.WriteLine($"Your guess is too {result}.");
 }
+Console.WriteLine("\nPress any key to exit...");
+Console.ReadKey(true);
+Guess CheckGuess(int secret, int guess)
+{
+    if (guess == secret) return Guess.Exact;
+    if (guess < secret) return Guess.Low;
+    return Guess.High;
+}
+int ReadInt(string prompt)
+{
+    while (true)
+    {
+        Console.Write(prompt);
+        if (int.TryParse(Console.ReadLine(), out int result))
+            return result;
+    }
+}
+int getMax(Mode mode)
+{
+    switch (mode)
+    {
+        case Mode.Easy: return 10;
+        case Mode.Medium: return 100;
+        default: return 1000;
+    }
+}
+Mode GetMode()
+{
+    Console.Write("Select a Mode (E)asy, (M)edium, (H)ard: ");
+    while (true)
+    {
+        var key = Console.ReadKey(true).Key;
+        switch (key)
+        {
+            case ConsoleKey.E: return Mode.Easy;
+            case ConsoleKey.M: return Mode.Medium;
+            case ConsoleKey.H: return Mode.Hard;
+        }
+    }
+}
+enum Mode { Easy, Medium, Hard }
+enum Guess { Low, High, Exact }
