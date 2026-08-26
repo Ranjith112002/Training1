b5df06e41a2f7f031e8ccde5c88de494c0f9a4ff
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-08-26 12:54:07 +0530

    Updated the logic to stick strictly to 1–100 with a 7-guess limit;

--- A01/Program.cs
+++ A01/Program.cs
@@ -1,20 +1,25 @@
-﻿var mode = GetMode ();
-Console.WriteLine (mode);
-int max = getMax (mode);
+﻿int max = 100;
+int maxTries = 7;
 int secret = Random.Shared.Next (1, max + 1);
 Console.WriteLine ($"I've thought of a number between 1 to {max}.");
-Console.WriteLine ("Try to guess it");
-for (int tries = 1; ; tries++) {
+Console.WriteLine ($"Try to guess it in {maxTries} tries or less.");
+bool guessed = false;
+for (int tries = 1; tries <= maxTries; tries++) {
    int guess = ReadInt ("> ");
    var result = CheckGuess (secret, guess);
    if (result == Guess.Exact) {
       Console.WriteLine ($"You guessed in {tries} tries.");
+      guessed = true;
       break;
    }
    Console.WriteLine ($"Your guess is too {result}.");
 }
+if (!guessed) {
+   Console.WriteLine ($"Game over! You couldn't guess the number. It was {secret}.");
+}
 Console.WriteLine ("\nPress any key to exit...");
 Console.ReadKey (true);
+
 Guess CheckGuess (int secret, int guess) {
    if (guess == secret) return Guess.Exact;
    if (guess < secret) return Guess.Low;
@@ -27,23 +32,4 @@ int ReadInt (string prompt) {
          return result;
    }
 }
-int getMax (Mode mode) {
-   switch (mode) {
-      case Mode.Easy: return 10;
-      case Mode.Medium: return 100;
-      default: return 1000;
-   }
-}
-Mode GetMode () {
-   Console.Write ("Select a Mode (E)asy, (M)edium, (H)ard: ");
-   while (true) {
-      var key = Console.ReadKey (true).Key;
-      switch (key) {
-         case ConsoleKey.E: return Mode.Easy;
-         case ConsoleKey.M: return Mode.Medium;
-         case ConsoleKey.H: return Mode.Hard;
-      }
-   }
-}
-enum Mode { Easy, Medium, Hard }
 enum Guess { Low, High, Exact }
