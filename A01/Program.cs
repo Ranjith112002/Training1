d3a5f678fc69e52d72df843940c4a60ce1c657b0
Shanmugam <Ranjithkumar.Shanmugam@trumpf.com>
2026-08-28 12:21:07 +0530

    I have modify the code;

--- A01/Program.cs
+++ A01/Program.cs
@@ -1,35 +1,22 @@
-﻿int max = 100;
-int maxTries = 7;
-int secret = Random.Shared.Next (1, max + 1);
-Console.WriteLine ($"I've thought of a number between 1 to {max}.");
-Console.WriteLine ($"Try to guess it in {maxTries} tries or less.");
-bool guessed = false;
-for (int tries = 1; tries <= maxTries; tries++) {
-   int guess = ReadInt ("> ");
-   var result = CheckGuess (secret, guess);
+﻿int secret = Random.Shared.Next (1, 101);
+Console.WriteLine ("Think of a number between 1 and 100");
+for (int tries = 1; tries <= 7; tries++) {
+   int guess;
+   while (!int.TryParse (Console.ReadLine (), out guess)) {
+   }
+   Guess result = CheckGuess (secret, guess);
    if (result == Guess.Exact) {
       Console.WriteLine ($"You guessed in {tries} tries.");
-      guessed = true;
       break;
    }
    Console.WriteLine ($"Your guess is too {result}.");
+   if (tries == 7) {
+      Console.WriteLine ($"Game over! You couldn't guess the number. It was {secret}.");
+   }
 }
-if (!guessed) {
-   Console.WriteLine ($"Game over! You couldn't guess the number. It was {secret}.");
-}
-Console.WriteLine ("\nPress any key to exit...");
-Console.ReadKey (true);
-
 Guess CheckGuess (int secret, int guess) {
    if (guess == secret) return Guess.Exact;
    if (guess < secret) return Guess.Low;
    return Guess.High;
 }
-int ReadInt (string prompt) {
-   while (true) {
-      Console.Write (prompt);
-      if (int.TryParse (Console.ReadLine (), out int result))
-         return result;
-   }
-}
-enum Guess { Low, High, Exact }
+enum Guess { Low,High,Exact}
